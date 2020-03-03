6988b527131458b7ed3f82ff005dcb7221dbe78d
Steven <sz64@bu.edu>
2020-03-03 18:54:17 -0500

    created template of processor

---
+++ processor_steven/processor.py
@@ -0,0 +1,11 @@
+# from sensors_brian import sensorData as s
+import time
+
+def processData():
+    # Any type of data manipulation
+    while True:
+        # get settings
+        # send data to display and other relevant settings
+        time.sleep(settings['interval']);
+    return False
+        
