0662a3bc489760bffd436f8cb18704c8a5d2675d
Steven Zhu <sz64@bu.edu>
2020-03-18 09:31:24 -0400

    more edits to processor

--- processor_steven/processor.py
+++ processor_steven/processor.py
@@ -6,32 +6,33 @@ import time
 global current_alerts;
 
 def processData():
-    global current_alerts;
-    try:
-        data = genNumbers();
-    except:
-        print('Error retrieving data.')
-        return False;
-    try: 
-        time_setting = time_display_change();
-    except:
-        print('Error retrieving time settings.');
-        return False;
-    try:
-        vital_trend = ai_thoughts(healthmonitor.csv);
-    except:
-        print('Error retrieving time settings.');
-        # return False;
-    # Any type of data manipulation
-    
-    # What is returned?
-    return True;
+	global current_alerts;
+	try:
+		data = genNumbers();
+	except:
+		print('Error retrieving data.')
+		return False;
+	try: 
+		time_setting = time_display_change();
+	except:
+		print('Error retrieving time settings.');
+		return False;
+	try:
+		vital_trend = ai_thoughts(healthmonitor.csv);
+	except:
+		print('Error retrieving time settings.');
+		# return False;
+	# Any type of data manipulation
+	
+	# What is returned?
+	return True;
 
 def update_alert_status(alerts):
-    # Updates a global variable with the current alert status
-    # that the processor uses.
-    # Could also write it to a file and read it so that this 
-    # does not need to be run as a thread. 
-    global current_alerts;
-    current_alerts = alerts;
-    return True;
+	# Updates a global variable with the current alert status
+	# that the processor uses.
+	global current_alerts;
+	current_alerts = alerts;
+	return True;
+	
+# if __name__ == '__main__':
+	# processData():
